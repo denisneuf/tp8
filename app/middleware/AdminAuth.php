6e311ae858723e89b6b03b0f51d260770475e03e
denisneuf <denisneuf@hotmail.com>
2025-09-22 20:11:05 +0800

    AdminAuthv2

--- app/middleware/AdminAuth.php
+++ app/middleware/AdminAuth.php
@@ -12,26 +12,11 @@ class AdminAuth
     public function handle($request, \Closure $next)
     {
         if (!Session::has('admin_user')) {
-            //return Redirect::url('admin/login');
-            //return redirect(url('admin/login'))->with('error', 'Debes iniciar sesión');
-
         	$currentUrl = $request->url(true); // URL completa
-            //dump($currentUrl);
-            //Log::debug('URL: ' . currentUrl);
 			$encodedUrl = base64_encode($currentUrl);
-
-
-        	//dump($currentUrl);
-
-
         	return redirect('/admin/login?redirect=' . urlencode($encodedUrl))
             	->with('error', 'Debes iniciar sesión');
-
-
-            //return redirect('/admin/login')->with('error', 'Debes iniciar sesión');
         }
-
         return $next($request);
     }
 }
-
